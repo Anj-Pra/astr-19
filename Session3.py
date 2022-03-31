538ab25588d8394f6c0c2e49bc14a18ee2f2b098
Anj-Pra <102757508+Anj-Pra@users.noreply.github.com>
2022-03-31 12:33:36 -0700

    Add Session 3 Files

---
+++ Session3.py
@@ -0,0 +1,19 @@
+# Session 2 Prompt
+
+# Write a Python program that defines a function f(x) 
+# that returns x**3 + 8. In the main function of the 
+# program, call f(x) with x = 9 and print the result. 
+# Use an if statement that executes if the result is 
+# larger than 27 and prints "YAY!".
+
+def f(x): # do some math
+    return(x**3 + 8)
+
+def main(): # main function
+    x = 9
+    result = f(x)
+    if result > 27:
+        print("YAY!")
+
+if __name__ == "__main__":
+    main()
