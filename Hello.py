2825d2221635782b768cf7fb6156f01a6527c03c
Anj-Pra <102757508+Anj-Pra@users.noreply.github.com>
2022-03-31 11:02:48 -0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,10 @@
+# This program will write Hello World
+
+def printHelloWorld(): # function to print
+    print("\nHello Fam!")
+
+def main(): # main function
+    printHelloWorld()
+
+if __name__ == "__main__":
+    main()
